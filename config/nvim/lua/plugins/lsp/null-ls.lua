202b43a00e3b7acc41c05486c86deef857e8db22
Sandaruwan Silva <san@resample.net>
2022-09-11 16:00:27 +0530

    snippets and refactoring
    Signed-off-by: Sandaruwan Silva <san@resample.net>

---
+++ config/nvim/lua/plugins/lsp/null-ls.lua
@@ -0,0 +1,14 @@
+null_ls = require('null-ls')
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup {
+  sources = {
+    formatting.prettierd,
+    formatting.black,
+    -- formatting.yapf,
+    formatting.stylua,
+    diagnostics.flake8,
+  },
+}
